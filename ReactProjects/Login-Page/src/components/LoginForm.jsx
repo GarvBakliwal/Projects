9522b66d213a9b7095d7b095aff457c86c34d97a
GarvBakliwal <garvbakliwal04@gmail.com>
2024-09-08 19:50:23 +0530

    Login-Form using React Complete

--- ReactProjects/Login-Page/src/components/LoginForm.jsx
+++ ReactProjects/Login-Page/src/components/LoginForm.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/LoginForm.css'
 const LoginForm = () => {
+  const [submitAction,setSubmitAction] = useState('Login');
+  const [footerText,setfooterText] = useState('Don\'t have an Account?');
+  const [buttonText,setButtonText] = useState('Sign up for free');
+  const [visibility,setVisibility] = useState('none');
+  const [hidden,setHidden] = useState('flex');
+  const [welcomeText,setWelcomeText] = useState('Welcome back! Enter your Details')
+  const handleClick = ()=>{
+    if (submitAction==='Login') {
+      setSubmitAction('Register')
+      setWelcomeText('Create an account to get Started!')
+      setButtonText('Sign in')
+      setfooterText('Already have an Account? ')
+      setVisibility('block')
+      setHidden('none')
+    } else if (submitAction==='Register') {
+      setSubmitAction('Login')
+      setWelcomeText('Welcome back! Enter your Details')
+      setButtonText('Sign up for free')
+      setfooterText('Don\'t have an Account?')
+      setVisibility('none')
+      setHidden('flex')
+    }
+  }
   return (
-    <div>LoginForm</div>
+    <div className='container'>
+      <h2>{submitAction}</h2>
+      <p>{welcomeText}</p>
+      <form className='input-form'>
+        <input type="text" placeholder='First Name' className="input-box" style={{display:visibility}} />
+        <input type="text" placeholder='Last Name' className="input-box" style={{display:visibility}}/>
+        <input type="email" placeholder='Email' className="input-box" />
+        <input type="password" placeholder='Password' className="input-box" />
+        <div className='input-remember'style={{display:hidden}}>
+          <div id='remember-me-checkbox' >
+            <input type="checkbox" name="" id="" />
+            <label htmlFor="">Remember me for 30 days</label>
+          </div>
+          <p><a href="#">Forgot Password ?</a></p>
+        </div>
+        <div className='submit-buttons'>
+          <button>{submitAction}</button>
+          {/* <button>Register</button> */}
+          <p>or</p>
+          <button>Sign in with Google</button>
+        </div>
+      </form>
+      <div className="footer">
+        {footerText} <button className='register-button' onClick={()=>handleClick()}>{buttonText}</button>
+      </div>
+    </div>
   )
 }
 
--- ReactProjects/Login-Page/src/components/MainRight.jsx
+++ ReactProjects/Login-Page/src/components/MainRight.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/MainRight.css'
 import LoginForm from './LoginForm'
 const MainRight = () => {
+  const [action,setAction] = useState('Login')
   return (
     <div className='container'>
       <div className='header'>
         Interactive Brand
       </div>
       <LoginForm/>
-      <div className="footer">
-        Dont have a account? <a href="#">Sign up for free</a>
-      </div>
+      
     </div>
   )
 }
