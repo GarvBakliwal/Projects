48dab02457bc9e32b60e61fa4c89325dc2448adf
GarvBakliwal <garvbakliwal04@gmail.com>
2024-09-19 00:45:32 +0530

    All User API COmplete

--- FakeStoreApi/shopping-site-backend/controller/userController.js
+++ FakeStoreApi/shopping-site-backend/controller/userController.js
@@ -6,15 +6,57 @@ exports.create = async (req, res) => {
         const { email } = req.body;
         const isExistingUser = await userData.findOne({ email });
         if (isExistingUser) {
-           return res.status(409).send("User Already Exists")
+            return res.status(409).send("User Already Exists")
         }
         const newUser = await userData.create(req.body);
         if (newUser) {
-           return res.status(201).json({
+            return res.status(201).json({
                 message: "User Created Successfully",
                 data: newUser
             })
         }
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
+exports.read = async (req, res) => {
+    try {
+        const allUsers = await userData.find();
+        res.status(200).json(allUsers);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
+exports.readOne = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const singleUser = await userData.findById({ _id: id });
+        if (singleUser) {
+            return res.status(200).json(singleUser)
+        }
+        res.status(404).send("User not Found");
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
+exports.updateUser = async (req, res) => {
+    const { id } = req.params;
+    // console.log("get body", req.body);
+    // console.log("get id", id);
+    try {
+        const updatedUser = await userData.findByIdAndUpdate(id, req.body, {
+            new: true
+        });
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
+exports.deleteUser = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletedUser = await userData.findByIdAndDelete(id);
+        res.status(200).json(deletedUser);
     } catch (error) {
         res.status(400).send(error);
     }
--- FakeStoreApi/shopping-site-backend/routes/userRoute.js
+++ FakeStoreApi/shopping-site-backend/routes/userRoute.js
@@ -3,5 +3,9 @@ const userController = require("../controller/userController.js");
 const router = e.Router();
 
 router.post('/create',userController.create);
+router.get('/',userController.read);
+router.get('/:id',userController.readOne);
+router.patch('/update/:id',userController.updateUser);
+router.delete('/delete/:id',userController.deleteUser);
 
 module.exports = router;
