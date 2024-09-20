9b2417a8beab93c1d93e76d439e1a10dcbacd824
GarvBakliwal <garvbakliwal04@gmail.com>
2024-09-20 14:51:36 +0530

    Auth Form Use Form & Resolver Complete

--- ReactProjects/Login-Page/src/components/AuthForm.jsx
+++ ReactProjects/Login-Page/src/components/AuthForm.jsx
@@ -3,6 +3,8 @@ import "../styles/AuthForm.css"; // Import your CSS
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGooglePlusG, faFacebookF, faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 const AuthForm = () => {
   const [isActive, setIsActive] = useState(false); // State to manage form toggle
 
@@ -11,10 +13,17 @@ const AuthForm = () => {
     setIsActive(!isActive);
   };
 
-  const {register,handleSubmit} = useForm();
+  const schema = z.object({
+    name: z.string().min(1, "Name is required").max(40, "Name cannot exceed 40 characters"),
+    email: z.string().email("Invalid email format"),
+    phone: z.string().min(10, "Phone number must be at least 10 digits").max(15, "Phone number cannot exceed 15 digits"),
+    password: z.string().min(6, "Password must be at least 6 characters")
+  });
+
+  const { register, formState : {errors} ,handleSubmit } = useForm({resolver:zodResolver(schema)});
 
   const onSubmit = (data) => console.log(data);
-  
+
 
   return (
     <div className={`container ${isActive ? "active" : ""}`} id="container">
@@ -30,9 +39,17 @@ const AuthForm = () => {
           </div>
           <span>Or Use your Email for Registration</span>
           <input type="text" placeholder="Name" {...register('name')} />
+          {errors.name && <div id="error">{errors.name.message}</div>}
+
           <input type="email" placeholder="Email" {...register('email')} />
+          {errors.email && <div id="error">{errors.email.message}</div>}
+
           <input type="tel" placeholder="Phone" {...register('phone')} />
+          {errors.phone && <div id="error">{errors.phone.message}</div>}
+
           <input type="password" placeholder="Password" {...register('password')} />
+          {errors.password && <div id="error">{errors.password.message}</div>}
+
           <button type="submit">Sign up</button>
         </form>
       </div>
@@ -49,10 +66,10 @@ const AuthForm = () => {
           </div>
           <span>Or Use your Email & Password</span>
 
-          <input type="email" placeholder="Email" {...register('email')}/>
-
+          <input type="email" placeholder="Email" {...register('email')} />
+          {errors.email && <div id="error">{errors.email.message}</div>}
           <input type="password" placeholder="Password" {...register('password')} />
-
+          {errors.password && <div id="error">{errors.password.message}</div>}
           <a href="#">Forgot your Password?</a>
 
           <button type="submit">Sign in</button>
