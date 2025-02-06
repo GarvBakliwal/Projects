7268f95261c96d3073b8596ca163bda538c623d4
GarvBakliwal <garvbakliwal04@gmail.com>
2025-02-07 00:03:47 +0530

    Sidebars Completed

--- fintechhub/app/(root)/page.tsx
+++ fintechhub/app/(root)/page.tsx
@@ -1,26 +1,34 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSide from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = {firstName:'Garv'};
+  const loggedIn = { firstName: 'Garv', lastName: 'Bakliwal', email:'garv@bakliwal.com' };
   return (
     <section className='home' >
       <div className="home-content">
         <header className='home-header'>
           <HeaderBox
-          type = "greeting"
-          title = "Welcome"
-          user={loggedIn?.firstName||"Guest"}
-          subtext = "Access and manage your Account and Transactions efficiently."
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your Account and Transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={5720.04}
           />
         </header>
-        <TotalBalanceBox
-        accounts={[]}
-        totalBanks={1}
-        totalCurrentBalance={5720.04}
-        />
+
+        RECENT TRANSACTIONS
       </div>
+      <RightSide
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance:123.50},{currentBalance:321.05}]}
+      />
     </section>
   )
 }
